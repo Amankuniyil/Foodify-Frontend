bfea87a6151896cf8ea005b76fc0fda87529b969
Aman Ahmed <amanahmedbarca@gmail.com>
2023-12-15 10:22:50 +0530

    new razor 3

--- src/Components/Restaurants/Notify.jsx
+++ src/Components/Restaurants/Notify.jsx
@@ -25,7 +25,9 @@ const Notify = () => {
   useEffect(() => {
     // setup chat socket
     // const notifySocket = new WebSocket(`ws://127.0.0.1:8000/ws/notify/${Resid}/`);
-    const notifySocket = new WebSocket(`ws://foodify-frontend-five.vercel.app/ws/notify/${Resid}/`);
+    // const notifySocket = new WebSocket(`ws://foodify-frontend-five.vercel.app/ws/notify/${Resid}/`);
+    const notifySocket = new WebSocket(`wss://foodify-frontend-five.vercel.app/ws/notify/${Resid}/`);
+
 
     
 
